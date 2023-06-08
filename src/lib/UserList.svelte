48125b89b1e79aa2b486256ac5f9d0e5efb2e9ce
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-06-08 18:12:21 +0200

    hard mode ok

--- src/lib/UserList.svelte
+++ src/lib/UserList.svelte
@@ -1,22 +1,40 @@
 <script>
-  import { onMount } from "svelte";
-
-    export let users = [];
-
+    import { onMount } from "svelte";
+  
+    let name = "";
+  
     onMount(() => {
-        fetch('https://random-data-api.com/api/v2/users')
+      fetch('https://random-data-api.com/api/v2/users')
         .then(response => response.json())
         .then(data => {
-            users = data.map((/** @type {any} */ user) => '${user.first_name} ${user.last_name}');
+          const { first_name, last_name } = data;
+          name = `${first_name} ${last_name}`;
         });
-  });
-</script>
+    });
+  </script>
+  
+  <main>
+    <div>
+        <h1>{name}</h1>
+    </div>
+  </main>
 
-<main>
-    <ul>
-        {#each users as user}
-        <li>{user}</li>
-        {/each}
-    </ul>
-</main>
+  <style>
+    main {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+    }
+
+    div {
+        padding: 20px;
+        border: 2px solid gold;
+    }
+
+    h1 {
+        text-align: start;
+        margin-left: 15%;
+    }
+  </style>
+
   
